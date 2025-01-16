ea33f9481b4951f5895333f4e2e53ab52e7bcf42
macmini46 <macmini46@macmini-46.local>
2025-01-15 21:18:03 -0800

    add: header with a toggle functionality

---
+++ components/header/Header.js
@@ -0,0 +1,93 @@
+'use client';
+import Link from 'next/link';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const path = usePathname();
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false); 
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+
+  if (!mounted) return null;
+
+  return (
+    <header className="border-b-2 bg-white dark:bg-gray-900">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <Link
+          href="/"
+          className="text-lg sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Blogify
+          </span>
+        </Link>
+
+        <div className="flex items-center gap-8">
+          <nav className="flex gap-4">
+            <Link
+              href="/"
+              className={`px-4 py-2 rounded-lg ${
+                path === '/'
+                  ? 'bg-indigo-500 text-white'
+                  : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
+              }`}
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className={`px-4 py-2 rounded-lg ${
+                path === '/about'
+                  ? 'bg-indigo-500 text-white'
+                  : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
+              }`}
+            >
+              About
+            </Link>
+            <Link
+              href="/projects"
+              className={`px-4 py-2 rounded-lg ${
+                path === '/projects'
+                  ? 'bg-indigo-500 text-white'
+                  : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
+              }`}
+            >
+              Posts
+            </Link>
+          </nav>
+
+          <form className="relative hidden lg:block">
+            <input
+              type="text"
+              placeholder="Search..."
+             
+              className="px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+            />
+            <button
+              type="submit"
+              className="absolute right-2 top-2 text-gray-500 dark:text-gray-300"
+            >
+              <AiOutlineSearch size={20} />
+            </button>
+          </form>
+        </div>
+
+        <button
+          className="ml-4 p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300"
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+          {theme === 'light' ? <FaSun size={18} /> : <FaMoon size={18} />}
+        </button>
+      </div>
+    </header>
+  );
+}
