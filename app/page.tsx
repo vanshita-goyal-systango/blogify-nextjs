fe7a46524fcb305fef44cc58fd7195bf21bc8e22
Vanshita Goyal <vanshita.goyal@systango.com>
2025-01-22 00:58:28 -0800

    Fix

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { redirect } from "next/navigation";
+
+const page = () => {
+  redirect("/users")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
